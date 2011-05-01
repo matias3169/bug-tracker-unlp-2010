796f28447de3eae8c412989e6f4da6e6585b48c9
mjaindu@hotmail.com <mjaindu@hotmail.com@549e7085-dc08-dea5-06f5-abb3ee9dd9f0>
2011-05-01 01:44:28 +0000

    clase miembro

---
+++ src/unlp/edu/core/Miembro.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package unlp.edu.core;
+
+/**
+ * @author G2_UNLP
+ * @version 1.0.0
+ *
+ */
+public class Miembro {
+
+	private Usuario usuario;
+	private Role role;
+	private Proyecto proyecto;
+	
+	public Miembro(Usuario usuario, Role role, Proyecto proyecto){
+		this.setUsuario(usuario);
+		this.setRole(role);
+		this.setProyecto(proyecto);
+	}
+	/**
+	 * @return the usuario
+	 */
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	/**
+	 * @param usuario the usuario to set
+	 */
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	/**
+	 * @return the rol
+	 */
+	public Role getRole() {
+		return role;
+	}
+	/**
+	 * @param rol the rol to set
+	 */
+	public void setRole(Role role) {
+		this.role = role;
+	}
+	/**
+	 * @return the proyecto
+	 */
+	public Proyecto getProyecto() {
+		return proyecto;
+	}
+	/**
+	 * @param proyecto the proyecto to set
+	 */
+	public void setProyecto(Proyecto proyecto) {
+		this.proyecto = proyecto;
+	}
+
+}
