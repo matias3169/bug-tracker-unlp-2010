aaba28dc3aabb6152e7d38a989ecf0547b818850
mjaindu@hotmail.com <mjaindu@hotmail.com@549e7085-dc08-dea5-06f5-abb3ee9dd9f0>
2011-05-01 01:12:21 +0000

    se agregaron getters y setters

--- src/unlp/edu/core/Role.java
+++ src/unlp/edu/core/Role.java
@@ -3,6 +3,9 @@
  */
 package unlp.edu.core;
 
+import java.util.Collection;
+import java.util.HashSet;
+
 /**
  * @author G2_UNLP
  * @version 1.0.0
@@ -10,5 +13,41 @@ package unlp.edu.core;
  */
 public class Role {
 
-	public Role() {};
+	private String nombre;
+	private Collection<String> permisos;
+	
+	
+	public Role(String nombre, Collection<String> permisos) {
+		this.setNombre(nombre);
+		this.setPermisos(permisos);
+	}
+	
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/**
+	 * @return the permisos
+	 */
+	public Collection<String> getPermisos() {
+		return permisos;
+	}
+
+	/**
+	 * @param permisos the permisos to set
+	 */
+	public void setPermisos(Collection<String> permisos) {
+		this.permisos = permisos;
+	}
+
 }
--- src/unlp/edu/core/Sistema.java
+++ src/unlp/edu/core/Sistema.java
@@ -4,6 +4,7 @@
 package unlp.edu.core;
 
 import java.util.Collection;
+import java.util.HashSet;
 
 /**
  * @author G2_UNLP
@@ -13,11 +14,124 @@ import java.util.Collection;
 public class Sistema {
 	
 	private Collection<Proyecto> proyectos;
-	private Collection<Role> roles;
+	private Collection<Role> rolesSistema;
+	private Collection<Role> rolesProyecto;
 	private Collection<Usuario> usuarios;
-
+	
 	public Sistema () 
 	{
+		this.setProyectos(new HashSet<Proyecto>());
+		this.setUsuarios(new HashSet<Usuario>());
+		
+		this.setRolesSistema();
+		this.setRolesProyecto();
+
 		System.out.println("Sistema creado");
 	}
+	
+	/**
+	 * @return the proyectos
+	 */
+	public Collection<Proyecto> getProyectos() {
+		return proyectos;
+	}
+
+	/**
+	 * @param proyectos the proyectos to set
+	 */
+	public void setProyectos(Collection<Proyecto> proyectos) {
+		this.proyectos = proyectos;
+	}
+
+	/**
+	 * @return the roles
+	 */
+	public Collection<Role> getRolesSistema() {
+		return rolesSistema;
+	}
+
+	/**
+	 * @return the roles
+	 */
+	public Collection<Role> getRolesProyecto() {
+		return rolesProyecto;
+	}
+
+	/**
+	 * @return the usuarios
+	 */
+	public Collection<Usuario> getUsuarios() {
+		return usuarios;
+	}
+
+	/**
+	 * @param usuarios the usuarios to set
+	 */
+	public void setUsuarios(Collection<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	}
+
+	/**
+	 * Establece roles del sistema hardcodeados
+	 * @return Lista de roles del sistema
+	 */
+    private void setRolesSistema(){
+    	rolesSistema = new HashSet<Role>();
+    	
+    	rolesSistema.add(new Role("Admin",this.getPermisosSistema("Super")));
+    	rolesSistema.add(new Role("Developer",this.getPermisosSistema("Normal")));
+    	rolesSistema.add(new Role("Guest",this.getPermisosSistema("")));
+    }
+
+	/**
+	 * Establece roles del proyecto hardcodeados
+	 * @return Lista de roles del proyecto
+	 */
+    private void setRolesProyecto(){
+    	rolesProyecto = new HashSet<Role>();
+    	
+    	rolesProyecto.add(new Role("Lider de Proyecto",this.getPermisosProyecto("Super")));
+    	rolesProyecto.add(new Role("Desarrollador",this.getPermisosProyecto("Normal")));
+    	rolesProyecto.add(new Role("DBA",this.getPermisosProyecto("Normal")));
+    	rolesProyecto.add(new Role("Tester",this.getPermisosProyecto("Normal")));
+    }
+    
+    private HashSet<String> getPermisosSistema(String tipoUsuario){
+      	Collection<String> permisos = new HashSet<String>();
+    	
+    	if (tipoUsuario.equals("Super")){
+    		permisos.add("Creacion");
+    		permisos.add("Lectura");
+    		permisos.add("Escritura");
+    	}else {
+    		if (tipoUsuario.equals("Normal")){
+    			permisos.add("Escritura");
+    			permisos.add("Lectura");
+    		} else {
+    			permisos.add("Lectura");
+    		}
+    	}
+    
+    	return (HashSet<String>)permisos;
+    }
+    
+    private HashSet<String> getPermisosProyecto(String tipoUsuario){
+      	Collection<String> permisos = new HashSet<String>();
+    	
+    	if (tipoUsuario.equals("Super")){
+    		permisos.add("Creacion");
+    		permisos.add("Lectura");
+    		permisos.add("Escritura");
+    	}else {
+    		if (tipoUsuario.equals("Normal")){
+    			permisos.add("Escritura");
+    			permisos.add("Lectura");
+    		} else {
+    			permisos.add("Lectura");
+    		}
+    	}
+    
+    	return (HashSet<String>)permisos;
+    }
+    
 }
