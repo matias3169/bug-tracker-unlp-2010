c097b216d16d278a22f8fc162b5dc527f77af521
mjaindu@hotmail.com <mjaindu@hotmail.com@549e7085-dc08-dea5-06f5-abb3ee9dd9f0>
2011-05-07 15:00:54 +0000

    getters y setters

--- src/unlp/edu/core/Proyecto.java
+++ src/unlp/edu/core/Proyecto.java
@@ -3,6 +3,9 @@
  */
 package unlp.edu.core;
 
+import java.util.Collection;
+import java.util.HashSet;
+
 /**
  * @author G2_UNLP
  * @version 1.0.0
@@ -10,5 +13,71 @@ package unlp.edu.core;
  */
 public class Proyecto {
 
-	public Proyecto() {};
+	private String nombre;
+	private Collection<Item> items;
+	private Collection<TipoItem> tiposItems;
+	private Collection<Miembro> miembros;
+	
+	public Proyecto(String nombre) {
+		this.setNombre(nombre);
+		this.setItems(new HashSet<Item>());
+		this.setTiposItems(new HashSet<TipoItem>());
+		this.setMiembros(new HashSet<Miembro>());
+	}
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/**
+	 * @return the items
+	 */
+	public Collection<Item> getItems() {
+		return items;
+	}
+
+	/**
+	 * @param items the items to set
+	 */
+	public void setItems(Collection<Item> items) {
+		this.items = items;
+	}
+
+	/**
+	 * @return the tiposItems
+	 */
+	public Collection<TipoItem> getTiposItems() {
+		return tiposItems;
+	}
+
+	/**
+	 * @param tiposItems the tiposItems to set
+	 */
+	public void setTiposItems(Collection<TipoItem> tiposItems) {
+		this.tiposItems = tiposItems;
+	}
+
+	/**
+	 * @return the miembros
+	 */
+	public Collection<Miembro> getMiembros() {
+		return miembros;
+	}
+
+	/**
+	 * @param miembros the miembros to set
+	 */
+	public void setMiembros(Collection<Miembro> miembros) {
+		this.miembros = miembros;
+	};
 }
