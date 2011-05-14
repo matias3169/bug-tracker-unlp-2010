ece40462ef9aa3577636e3ddacfd613ea2c883dd
valebecker@gmail.com <valebecker@gmail.com@549e7085-dc08-dea5-06f5-abb3ee9dd9f0>
2011-05-14 22:39:03 +0000

    Modifique la clausula import.

--- trunk/src/unlp/edu/core/Main.java
+++ trunk/src/unlp/edu/core/Main.java
@@ -1,9 +1,9 @@
 package unlp.edu.core;
 
-import java.util.Calendar;
-import java.util.Collection;
-import java.util.Date;
-import java.util.HashSet;
+import java.util.*;
+//import java.util.Collection;
+//import java.util.Date;
+//import java.util.HashSet;
 
 public class Main {
 
@@ -23,6 +23,8 @@ public class Main {
 		estado2.agregarEstadoSiguiente(estado3);
 		estado3.agregarEstadoSiguiente(estado2);
 		estado3.agregarEstadoSiguiente(estado4);
+		System.out.print("estado1= ");
+		System.out.println(estado1.getDescripcion());
 		
 		estadosPosibles.add(estado1);
 		estadosPosibles.add(estado2);
@@ -40,23 +42,46 @@ public class Main {
 		Usuario usuario1 = new Usuario("Vale", roleSistema1);
 		Usuario usuario2 = new Usuario("Santi", roleSistema2);
 		Usuario usuario3 = new Usuario("Matias", roleSistema3);
+		Usuario usuario4 = new Usuario("Usuario4", roleSistema3);
 		
 		Proyecto proyecto = new Proyecto("PROYECTO1");
 		
 		Miembro miembro1 = new Miembro(usuario1, role1, proyecto);
 		Miembro miembro2 = new Miembro(usuario2, role2, proyecto);
 		Miembro miembro3 = new Miembro(usuario3, role3, proyecto);
+		Miembro miembro4 = new Miembro(usuario4, role3, proyecto);
 		
 		miembrosDisponibles.add(miembro1);
 		miembrosDisponibles.add(miembro2);
 		miembrosDisponibles.add(miembro3);
 		
 		TipoItem tipo1 = new TipoItem("Reporte de Bug",estado1,estadosPosibles);
-		Item item1=new Item("Corregir falla",tipo1);
-		
+		Item item1=new Item("Corregir falla",tipo1,0);
+	
 		item1.cambiarEstadoItem(estado1, miembro1, miembrosDisponibles);
+		System.out.print("estadoActual= ");
+		System.out.println(item1.getEstadoActual().getEstado().getDescripcion());
+		System.out.print("fechaInicio= ");
+		System.out.println(item1.getEstadoActual().getFechaInicio());
+		System.out.print("responsable= ");
+		System.out.println(item1.getEstadoActual().getResponsable().getUsuario().getNombre());
+		System.out.println();
 		
+		item1.cambiarEstadoItem(estado2, miembro4, miembrosDisponibles);
+		System.out.print("estadoActual= ");
+		System.out.println(item1.getEstadoActual().getEstado().getDescripcion());
+		System.out.print("fechaInicio= ");
+		System.out.println(item1.getEstadoActual().getFechaInicio());
+		System.out.print("responsable= ");
+		System.out.println(item1.getEstadoActual().getResponsable().getUsuario().getNombre());
+		System.out.println();
 		
-		
+		item1.cambiarResponsable(miembro3, miembrosDisponibles);
+		System.out.print("estadoActual= ");
+		System.out.println(item1.getEstadoActual().getEstado().getDescripcion());
+		System.out.print("fechaInicio= ");
+		System.out.println(item1.getEstadoActual().getFechaInicio());
+		System.out.print("responsable= ");
+		System.out.println(item1.getEstadoActual().getResponsable().getUsuario().getNombre());
 	}
 }
