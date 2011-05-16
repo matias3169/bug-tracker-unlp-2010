2386da8d6aaa520b65e3599164ded9f70c0c28fe
valebecker@gmail.com <valebecker@gmail.com@549e7085-dc08-dea5-06f5-abb3ee9dd9f0>
2011-05-16 01:18:23 +0000

    Borré el método cambiarEstado() porque no es necesario en esta clase.

--- trunk/src/unlp/edu/core/Estado.java
+++ trunk/src/unlp/edu/core/Estado.java
@@ -8,8 +8,7 @@ public class Estado {
 	private String descripcion;
 	private Collection<Estado> estadosSiguientes;
 	
-	public Estado(String descripcion)
-	{
+	public Estado(String descripcion){
 		this.descripcion= descripcion;
 		this.estadosSiguientes = new HashSet<Estado>();
 	}
@@ -34,15 +33,21 @@ public class Estado {
 	 * Metodos
 	 */
 
-	public Estado cambiarEstado(Estado e)
-	{
-		/**
-		 * ¿qué tiene que retornar???
-		 */
-		return e;
-	}
 	public void agregarEstadoSiguiente(Estado e)
 	{
 		this.estadosSiguientes.add(e);
+	}
+	
+	public void listarEstadosSiguientes(){
+		System.out.print("Estados siguientes del estado ");
+		System.out.print(this.getDescripcion());
+		System.out.println(":");
+		Iterator<Estado> it = estadosSiguientes.iterator();
+    	while(it.hasNext())
+    	{
+    		System.out.print(it.next().getDescripcion());
+    		System.out.print(", ");
+    	}
+    	System.out.println();
 	}
 }
