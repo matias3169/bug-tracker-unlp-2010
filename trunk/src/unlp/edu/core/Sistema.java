06bdee14a0ee301d2ae1e66009e84d62a53866b1
valebecker@gmail.com <valebecker@gmail.com@549e7085-dc08-dea5-06f5-abb3ee9dd9f0>
2011-05-16 01:28:06 +0000

    Agregue los métodos necesarios para que el sistema brinde la funcionalidad mínima indicada en el enunciado. Al cambiar el estado de un item falta la verificación de que sea un estado siguiente al actual.

--- trunk/src/unlp/edu/core/Sistema.java
+++ trunk/src/unlp/edu/core/Sistema.java
@@ -89,7 +89,7 @@ public class Sistema {
     private void setRolesProyecto(){
     	rolesProyecto = new HashSet<Role>();
     	
-    	rolesProyecto.add(new Role("Lider de Proyecto",this.getPermisosProyecto("Super")));
+    	rolesProyecto.add(new Role("Lider",this.getPermisosProyecto("Super")));
     	rolesProyecto.add(new Role("Desarrollador",this.getPermisosProyecto("Normal")));
     	rolesProyecto.add(new Role("DBA",this.getPermisosProyecto("Normal")));
     	rolesProyecto.add(new Role("Tester",this.getPermisosProyecto("Normal")));
@@ -117,7 +117,7 @@ public class Sistema {
     private HashSet<String> getPermisosProyecto(String tipoUsuario){
       	Collection<String> permisos = new HashSet<String>();
     	
-    	if (tipoUsuario.equals("Super")){
+    	if (tipoUsuario.equals("Lider")){
     		permisos.add("Creacion");
     		permisos.add("Lectura");
     		permisos.add("Escritura");
@@ -133,32 +133,110 @@ public class Sistema {
     	return (HashSet<String>)permisos;
     }
     
-    public void nuevoMiembro(Proyecto proyecto, Usuario usuario, Role rol){
+    public Miembro nuevoMiembro(Proyecto proyecto, Usuario usuario, Role rol){
     	Miembro miembro = new Miembro(proyecto, usuario, rol);
     	proyecto.agregarMiembro(miembro);
+    	return miembro;
     }
     
-    public void nuevoEstado(Proyecto proyecto, TipoItem tipoItem, String descripcion){
-    	proyecto.nuevoEstado(tipoItem, descripcion);
+    public Estado nuevoEstado(Proyecto proyecto, TipoItem tipoItem, String descripcion){
+    	return proyecto.nuevoEstado(tipoItem, descripcion);
     }
     
     public void nuevoProyecto(String nombre, Usuario usuario){
     	Proyecto proyecto = new Proyecto(nombre);
-    	Role rol = new Role("Lider",new HashSet<String>()); // Revisar como queremos poner el rol y los permisos
+    	Role rol = new Role("Lider",this.getPermisosProyecto("Lider"));
     	Miembro miembro = new Miembro(proyecto,usuario,rol);
     	proyecto.agregarMiembro(miembro);
     	this.proyectos.add(proyecto);
     }
     
-    public void nuevoTipoItem(String descripcion, Proyecto proyecto){
-    	proyecto.nuevoTipoItem(descripcion);
+    public TipoItem nuevoTipoItem(String descripcion, Proyecto proyecto){
+    	return proyecto.nuevoTipoItem(descripcion);
     }
     
-    public void nuevoUsuario(String nombre, Role rolSistema){
-    	this.usuarios.add(new Usuario(nombre, rolSistema));
+    public Usuario nuevoUsuario(String nombre, Role rolSistema){
+    	Usuario usuario = new Usuario(nombre, rolSistema);
+    	this.usuarios.add(usuario);
+    	return usuario;
+    }
+    
+    public Role nuevoRolSistema(String descripcion){
+    	Collection<String> permisos = getPermisosSistema(descripcion); 
+    	Role rol = new Role(descripcion, permisos);
+    	this.rolesSistema.add(rol);
+    	return rol;
+    }
+    
+    public Role nuevoRolProyecto(String descripcion){
+    	Collection<String> permisos = getPermisosSistema(descripcion); 
+    	Role rol = new Role(descripcion, permisos);
+    	this.rolesProyecto.add(rol);
+    	return rol;
+    }
+    
+    public Item nuevoItem(Proyecto proyecto, String descripcion, TipoItem tipo, int prioridad2){
+    	return proyecto.nuevoItem(descripcion, tipo, prioridad2);
     }
     
     public void cambiarEstadoItem(Proyecto proyecto, Item item, Estado estado, Miembro responsable, Collection<Miembro> miembrosDisponibles){
     	proyecto.cambiarEstadoItem(item, estado, responsable, miembrosDisponibles);
+    }
+    
+    public void agregarEstadoSiguiente(Proyecto proyecto, TipoItem tipo, Estado estadoI, Estado estadoF){
+    	proyecto.agregarEstadoSiguiente(tipo, estadoI, estadoF);
+    }
+    
+    public String verEstadoActualItem(Proyecto proyecto, Item item){
+    	return proyecto.verEstadoActualItem(item);
+    }
+    
+    public void listarUsuarios(){
+    	System.out.println("Usuarios del sistema:");
+    	Iterator<Usuario> it = usuarios.iterator();
+    	while(it.hasNext())
+    	{
+    		System.out.print(it.next().getNombre());
+    		System.out.print(", ");
+    	}
+    	System.out.println();
+    }
+    
+    public void listarRolesSistema(){
+    	System.out.println("Roles de sistema:");
+    	Iterator<Role> it = rolesSistema.iterator();
+    	while(it.hasNext())
+    	{
+    		System.out.print(it.next().getNombre());
+    		System.out.print(", ");
+    	}
+    	System.out.println();
+    }
+    
+    public void listarRolesProyecto(){
+    	System.out.println("Roles de proyecto:");
+    	Iterator<Role> it = rolesProyecto.iterator();
+    	while(it.hasNext())
+    	{
+    		System.out.print(it.next().getNombre());
+    		System.out.print(", ");
+    	}
+    	System.out.println();
+    }
+    
+    public void listarTiposItem(Proyecto proyecto){
+    	proyecto.listarTiposItem();
+    }
+    
+    public void listarItems(Proyecto proyecto){
+    	proyecto.listarItems();
+    }
+    
+    public void listarEstadosPosibles(Proyecto proyecto, TipoItem tipo){
+    	proyecto.listarEstadosPosibles(tipo);
+    }
+    
+    public void listarEstadosSiguientes(Proyecto proyecto, TipoItem tipo, Estado estado){
+    	proyecto.listarEstadosSiguientes(tipo, estado);
     }
 }
