8e6289cc19e7e160f0fc7165b51d048d01cbbbfe
mjaindu@hotmail.com <mjaindu@hotmail.com@549e7085-dc08-dea5-06f5-abb3ee9dd9f0>
2011-08-19 03:22:36 +0000

    nuevos Actions

---
+++ src/unlp/edu/action/EditarProyectoAction.java
@@ -0,0 +1,42 @@
+package unlp.edu.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.*;
+
+import unlp.edu.core.Proyecto;
+import unlp.edu.core.Sistema;
+import unlp.edu.core.Miembro;;
+
+public class EditarProyectoAction extends Action{
+
+	/* (non-Javadoc)
+	 * @see org.apache.struts.action.Action#execute(org.apache.struts.action.ActionMapping, org.apache.struts.action.ActionForm, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	public ActionForward execute(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		DynaActionForm editarProyectoForm = (DynaActionForm) form;
+		ActionErrors errors = new ActionErrors();
+		
+		// Extraemos los datos del formulario 
+		String  nombreActualProyecto = (String) editarProyectoForm.get("nombreActualProyecto");
+		//String  liderActualProyecto = (String) editarProyectoForm.get("liderActualProyecto");
+		
+		String  nuevoNombreProyecto = (String) editarProyectoForm.get("nombreProyecto");
+		String  nuevoLiderProyecto = (String) editarProyectoForm.get("liderProyecto");
+		
+		Sistema sistema = Sistema.getInstance();
+		Proyecto proyecto = sistema.getProyectoPorNombre(nombreActualProyecto);
+		proyecto.setNombre(nuevoNombreProyecto);
+		Miembro liderProyecto = sistema.getMiembro(proyecto, nuevoLiderProyecto);
+		proyecto.setLiderProyecto(liderProyecto);
+		
+		// Mostramos la siguiente vista
+		return mapping.findForward("ok"); 
+		
+	}
+
+}
---
+++ src/unlp/edu/action/EliminarProyectoAction.java
@@ -0,0 +1,48 @@
+package unlp.edu.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.*;
+
+import unlp.edu.core.Proyecto;
+import unlp.edu.core.Sistema;
+import unlp.edu.core.Miembro;;
+
+public class EliminarProyectoAction extends Action{
+
+	/* (non-Javadoc)
+	 * @see org.apache.struts.action.Action#execute(org.apache.struts.action.ActionMapping, org.apache.struts.action.ActionForm, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	public ActionForward execute(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		DynaActionForm eliminarProyectoForm = (DynaActionForm) form;
+		ActionErrors errors = new ActionErrors();
+		
+		// Extraemos los datos del formulario 
+		String  nombreProyecto = (String) eliminarProyectoForm.get("nombreProyecto");
+		
+		Sistema sistema = Sistema.getInstance();
+		Proyecto proyecto = sistema.getProyectoPorNombre(nombreProyecto);
+		if (proyecto != null)
+		{
+			if (sistema.eliminarProyecto(proyecto))
+			{
+				// Mostramos la siguiente vista
+				return mapping.findForward("ok");
+			} else {
+				errors.add(ActionErrors.GLOBAL_MESSAGE,new ActionMessage("fallo.eliminar"));
+				saveErrors(request, errors);
+				return mapping.findForward("error");			
+			}
+		} else{
+			errors.add(ActionErrors.GLOBAL_MESSAGE,new ActionMessage("fallo.eliminar"));
+			saveErrors(request, errors);
+			return mapping.findForward("error");			
+		}
+		
+	}
+
+}
