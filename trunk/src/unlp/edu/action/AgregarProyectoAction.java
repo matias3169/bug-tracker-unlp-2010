c4f5da7347104a07d36f700264e1d36548455b63
mjaindu@hotmail.com <mjaindu@hotmail.com@549e7085-dc08-dea5-06f5-abb3ee9dd9f0>
2011-08-22 22:20:32 +0000

    actions para agregar proyecto y usuario

---
+++ trunk/src/unlp/edu/action/AgregarProyectoAction.java
@@ -0,0 +1,35 @@
+package unlp.edu.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.*;
+
+import unlp.edu.core.Sistema;
+
+public class AgregarProyectoAction extends Action{
+
+	/* (non-Javadoc)
+	 * @see org.apache.struts.action.Action#execute(org.apache.struts.action.ActionMapping, org.apache.struts.action.ActionForm, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	public ActionForward execute(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		DynaActionForm agregarProyectoForm = (DynaActionForm) form;
+		ActionErrors errors = new ActionErrors();
+		
+		// Extraemos los datos del formulario 
+		String  nombreProyecto = (String) agregarProyectoForm.get("nombre_proyecto");
+		String  liderProyecto = (String) agregarProyectoForm.get("lider_proyecto");
+		
+		Sistema sistema = Sistema.getInstance();
+		
+		sistema.nuevoProyecto(nombreProyecto, sistema.getUsuario(liderProyecto));
+		
+		// Mostramos la siguiente vista
+		return mapping.findForward("ok"); 
+		
+	}
+
+}
---
+++ trunk/src/unlp/edu/action/AgregarUsuarioAction.java
@@ -0,0 +1,36 @@
+package unlp.edu.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.*;
+
+import unlp.edu.core.Sistema;
+
+public class AgregarUsuarioAction extends Action{
+
+	/* (non-Javadoc)
+	 * @see org.apache.struts.action.Action#execute(org.apache.struts.action.ActionMapping, org.apache.struts.action.ActionForm, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	public ActionForward execute(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		DynaActionForm agregarProyectoForm = (DynaActionForm) form;
+		ActionErrors errors = new ActionErrors();
+		
+		// Extraemos los datos del formulario 
+		String  nombreUsuario = (String) agregarProyectoForm.get("nombre_usuario");
+		String  claveUsuario = (String) agregarProyectoForm.get("clave_usuario");
+		String  rolUsuario = (String) agregarProyectoForm.get("rol_usuario");
+		
+		Sistema sistema = Sistema.getInstance();
+		
+		sistema.nuevoUsuario(nombreUsuario, claveUsuario, sistema.getRoleSistema(rolUsuario));
+		
+		// Mostramos la siguiente vista
+		return mapping.findForward("ok"); 
+		
+	}
+
+}
