a80089fd89efa7103e3036d7f3111de9aac58f48
valebecker@gmail.com <valebecker@gmail.com@549e7085-dc08-dea5-06f5-abb3ee9dd9f0>
2011-08-28 18:08:39 +0000

    Action para cambiar el estado de un item

---
+++ src/unlp/edu/action/CambiarEstadoAction.java
@@ -0,0 +1,49 @@
+package unlp.edu.action;
+
+import java.util.Date;
+import java.util.HashSet;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.struts.action.*;
+
+import unlp.edu.core.EstadoItem;
+import unlp.edu.core.Sistema;
+import unlp.edu.core.Proyecto;
+import unlp.edu.core.Item;
+import unlp.edu.core.Estado;
+import unlp.edu.core.Miembro;
+import unlp.edu.core.TipoItem;
+
+public class CambiarEstadoAction extends Action{
+	
+	public ActionForward execute(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		DynaActionForm cambiarEstadoForm = (DynaActionForm) form;
+		ActionErrors errors = new ActionErrors();
+		
+		// Extraemos los datos del formulario 
+		String  nombreProyecto = (String) cambiarEstadoForm.get("nombreProyecto");
+		String nombreItem = (String) cambiarEstadoForm.get("nombreItem");
+		String descNuevoEstado = (String) cambiarEstadoForm.get("descNuevoEstado");
+		String nomNuevoResponsable = (String) cambiarEstadoForm.get("nomNuevoResponsable");
+		
+		Sistema sistema = Sistema.getInstance();
+		
+		Proyecto proyecto = sistema.getProyectoPorNombre(nombreProyecto);
+		Item item = proyecto.getItem(nombreItem);
+		Estado nuevoEstado = item.getTipoItem().getEstado(descNuevoEstado);
+		Miembro nuevoResponsable = proyecto.getMiembro(nomNuevoResponsable);
+		
+		Date date = new Date();
+		//Hay que pasar la lista de miembros del estadoitem
+		item.cambiarEstadoItem(nuevoEstado, nuevoResponsable, new HashSet<Miembro>(), date);
+		
+		
+		// Mostramos la siguiente vista
+		return mapping.findForward("ok"); 
+		
+	}
+
+}
