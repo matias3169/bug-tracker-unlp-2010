e8ba9971e98a798bb583b378cdfb415dec866e65
mjaindu@hotmail.com <mjaindu@hotmail.com@549e7085-dc08-dea5-06f5-abb3ee9dd9f0>
2012-03-12 16:29:19 +0000

    primer version BD

---
+++ trunk/db/LoadRoles.sql
@@ -0,0 +1,14 @@
+﻿DELIMITER $$
+
+DROP PROCEDURE IF EXISTS `bugtrackerdb`.`LoadRoles` $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `LoadRoles`()
+BEGIN
+  INSERT INTO role(tipo,nombre,permisos) VALUES("Sistema","Administrador","Total");
+  INSERT INTO role(tipo,nombre,permisos) VALUES("Sistema","Desarrollador","Total");
+  INSERT INTO role(tipo,nombre,permisos) VALUES("Proyecto","Lider","Total");
+  INSERT INTO role(tipo,nombre,permisos) VALUES("Proyecto","Desarrollador","Total");
+  INSERT INTO role(tipo,nombre,permisos) VALUES("Proyecto","DBA","Total");
+  INSERT INTO role(tipo,nombre,permisos) VALUES("Proyecto","Tester","Total");
+END $$
+
+DELIMITER ;
